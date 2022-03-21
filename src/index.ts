95fd46e10b8f2fe0268742b65f519b41b58b52eb
Sergey-Alex <strotelevsergwyalex@gmail.com>
2022-03-21 21:46:05 +0300

    add structure project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('wokedd')
+import App from "./pages/app";
+
+const app = new App()
+
+app.run()
--- src/pages/app/index.ts
+++ src/pages/app/index.ts
@@ -1,12 +1,19 @@
+import MainPage from "../main";
+
 class App {
     private container : HTMLElement;
+    private initialPage : MainPage
 
     constructor() {
         this.container = document.body;
+        this.initialPage = new MainPage('main-page')
 
     }
 
     run(){
-
+        const mainPageHtml = this.initialPage.render()
+        this.container.append(mainPageHtml)
     }
 }
+
+export default App
